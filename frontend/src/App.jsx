1c0fd5d60874340c1d76620cf24714a2489f7bf9
sanjayahewage0103 <pethumhewage66@gmail.com>
2025-08-04 02:00:57 +0530

    Refactor App component to include NavigationBar and update route elements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
 import { Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home Page</h1>} />
-      <Route path="/login" element={<h1>Login</h1>} />
-      <Route path="/register" element={<h1>Register</h1>} />
-    </Routes>
+    <>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<h1 className="p-4">Welcome to HousePrice Prediction App</h1>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/predict" element={<h1 className="p-4">Prediction Form Page</h1>} />
+        <Route path="/about" element={<h1 className="p-4">About Page</h1>} />
+        <Route path="/dashboard" element={<h1 className="p-4">Admin Dashboard</h1>} />
+      </Routes>
+    </>
   );
 }
 
 export default App;
-
